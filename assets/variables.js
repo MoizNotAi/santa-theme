845fdb15fdfceca734bf6a4f75a5399300653717
abdulmoizrana <abdulmoizrana.ar@gmail.com>
2023-10-24 13:00:44 +0500

    feature: added packages dropdown all functionality

---
+++ assets/variables.js
@@ -0,0 +1,90 @@
+window.myApp = window.myApp || {};
+window.myApp.productsDes = [
+  `<div>
+<div style="display:flex; align-items:center">
+  <img src="" alt="close0">
+  <div
+    style="
+      color: #FFF;
+      leading-trim: both;
+      text-edge: cap;
+      font-family: Lato;
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 24px;
+    "
+  >
+    Video 1: 3 months before Christmas
+  </div>
+</div>
+<div style="display:flex; align-items:center">
+  <img src="{{ '/tickIcon.png' | asset_url }}" alt="true1">
+  <div
+    style="
+      color: #FFF;
+      leading-trim: both;
+      text-edge: cap;
+      font-family: Lato;
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 24px;
+    "
+  >
+  Video 2: 1-2 months before Christmas
+  </div>
+</div>
+<div style="display:flex; align-items:center">
+  <img src="{{ '/tickIcon.png' | asset_url }}" alt="true2">
+  <div
+    style="
+      color: #FFF;
+      leading-trim: both;
+      text-edge: cap;
+      font-family: Lato;
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 24px;
+    "
+  >
+  Video 3: 2-3 weeks before Christmas
+  </div>
+</div>
+<div style="display:flex; align-items:center">
+  <img src="{{ '/tickIcon.png' | asset_url }}" alt="true">
+  <div
+    style="
+      color: #FFF;
+      leading-trim: both;
+      text-edge: cap;
+      font-family: Lato;
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 24px;
+    "
+  >
+  Video 4: Christmas Eve
+  </div>
+</div>
+<div style="display:flex; align-items:center">
+  <img src="{{ '/tickIcon.png' | asset_url }}" alt="true">
+  <div
+    style="
+      color: #FFF;
+      leading-trim: both;
+      text-edge: cap;
+      font-family: Lato;
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 24px;
+    "
+  >
+  Video 5: On Christmas Day
+  </div>
+</div>
+</div>`,
+];
